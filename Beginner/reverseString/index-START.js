b0176391d02d25a8761e1b6ab0d0e0b4200ec72b
jerednav <80506833+jerednav@users.noreply.github.com>
2021-05-31 09:50:59 -0700

    Update index-START.js

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,11 +4,22 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+//Chaining in-built methods
 function reverseString(text) {
     return text.split("").reverse().join("")
 }
 
+//The For Loop Way
+function reverseString(text) {
+       let result = ""
+       
+       for(let i = text.length - 1; i>= 0; i--) {
+            result += text[i]
+       }
+    
+        return result
+    
+}
 
 
 module.exports = reverseString
