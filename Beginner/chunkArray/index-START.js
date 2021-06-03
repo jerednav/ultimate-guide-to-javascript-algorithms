50774cb7ab8c66dec5e2f8e07172b36d0517a676
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-03 15:52:35 -0700

    Update index-START.js

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -5,7 +5,17 @@
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+    let result = []
+    for (value of array) {
+      let lastArray = result[result.length - 1]
+         if !lastArray || lastArray.length == size {
+          result.push ([value])
+         } else {
+          lastArray.push(value)
+         }
+    }
+ 
+    return result
 }
 
 
