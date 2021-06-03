56f3983386dbc18f8035cc7e45d1d2cb53776267
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-03 08:39:59 -0700

    Update index-START.js

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,10 +5,45 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-
+// Direct Comparison
 
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    const sanitizeString = function (str) {
+        return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
+    }
+    return sanitizeString(stringA) == sanitizeString(stringB)
+}
+
+
+// Character Map Comparison
+function isAnagram(stringA, stringB) {
+    
+    function createCharMap(text) {
+        let charMap = {}
+
+        for (let char of text) {
+            if (charMap.hasOwnProperty(char)) {
+                charMap[char]++
+            } else {
+                charMap[char] = 1
+            }
+        }
+
+        return charMap
+    }
+    
+    if (stringA.length === stringB.length) {
+        let stringAmap = createCharMap(stringA)
+        let stringBmap = createCharMap(stringB)
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
 }
 
 
