0d65f33acdd3f1a7e577fc4083676694f47cb0d3
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-07 15:23:47 -0700

    Update index-START.js

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -7,12 +7,53 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
+// Using a for-loop to find the immediate larger value's position
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+    arr.sort((a, b) => {
+        return a - b
+    })
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) {            return i
+        }
+    }
+    return arr.length
 }
+
+
+// Using a for-loop to determine the number of smaller values
+function whereIBelong(arr, num) {
+    var counter = 0
+    for (i = 0; i < arr.length; i++) {
+        if (num > arr++[++i]) {
+            counter++;
+        }
+    } 
+    return counter
+}
+
+// Using a while loop
+
+function whereIBelong(arr, num) {
+    arr.sort((a, b) => {
+        return a - b
+    })
+    let counter = 0;
+    while (num > arr[counter]) {
+        counter++
+    }
+    return counter
+}
+
+// Find the index of the value directly
+function whereIBelong(arr, num) {
+    arr.push(num)
+    arr.sort((a, b) => a - b)
+    return arr.indexOf(num)
+}
+
+
+
 
 
 
