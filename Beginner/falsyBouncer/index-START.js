5cf2b355a3e502f1beff2fa788b4f9779d9b0f90
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-07 15:21:14 -0700

    Update index-START.js

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,27 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
+//using a for... of loop
+function falsyBouncer(array) {
+    let result =[]
+    //loop through with each array value
+    for (value of array){
+    // push into result if truthy
+        if(value){
+            result.push(value)
+        }
+    }
+    return result
+}
+
+// Using filter
 
 function falsyBouncer(array) {
-    // Code goes here
+    return array.filter((value) =>{
+      return Boolean(value)     })
 }
+
+
 
 
 module.exports = falsyBouncer
